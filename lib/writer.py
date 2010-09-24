52b515ebae4d4a95b2ccfa9c5e8da6e387685808
Aaron Westendorf <aaron@agoragames.com>
2010-09-24 11:01:56 -0400

    Stream-based writing

--- lib/writer.py
+++ lib/writer.py
@@ -2,7 +2,7 @@
 Definition of the Writer class.
 """
 
-from cStringIO import StringIO  # TODO: make 2.6 and 3.0 compatible
+#from cStringIO import StringIO  # TODO: make 2.6 and 3.0 compatible
 from struct import pack
 from time import mktime
 from datetime import datetime
@@ -14,46 +14,80 @@ class Writer(object):
   """
 
   def __init__(self):
-    self.out = StringIO()
-    self.bits = []
-    self.bitcount = 0
+    #self.out = StringIO()
+    #self.bits = []
+    #self.bitcount = 0
+    self._output_buffer = []
 
-  def _flushbits(self):
-    if self.bits:
-      for b in self.bits:
-        self.out.write(pack('B', b))
-      self.bits = []
-      self.bitcount = 0
+  #def _flushbits(self):
+  #  if self.bits:
+  #    for b in self.bits:
+  #      self.out.write(pack('B', b))
+  #    self.bits = []
+  #    self.bitcount = 0
 
-  def getvalue(self):
-    """
-    Get what's been encoded so far.
+  def flush(self, stream):
+    '''Flush the output to the stream. Stream must be a file object.'''
+    self._bitcount = 0
+    self._bitfield = 0
+    for cb,data in self._output_buffer:
+      cb( data, stream )
 
-    """
-    self._flushbits()
-    return self.out.getvalue()
+  # NOTE: This is no longer needed, but we need to rethink how Message._serialize_properties
+  # and Channel frame/method/content sending is done.  If necessary:
+  #   stream = StringIO()
+  #   flush(stream)
+  #   return stream.getvalue()
+  #def getvalue(self):
+  #  """
+  #  Get what's been encoded so far.
+  #  """
+  #  self._flushbits()
+  #  return self.out.getvalue()
+
+
+  # TODO: There's a copy of a bunch of type checks so that both the public and 
+  # private uses get a local, meaningful exception. Re-think how that works.
 
   def write(self, s):
     """
     Write a plain Python string, with no special encoding.
-
     """
-    self._flushbits()
-    self.out.write(s)
+    self._output_buffer.append( (self._write_str, s) )
+
+  def _write_str(self, s, stream):
+    '''Write the string to a stream.'''
+    self._flush_bits( stream )
+    stream.write(s)
 
   def write_bit(self, b):
     """
     Write a boolean value.
     """
+    self._output_buffer.append( (self._write_bit, b) )
+
+  def _write_bit(self, b, stream):
+    '''Write a bit to the output stream.  Will actually pool all the bits into
+    a byte and then flush when that's full.  More common case is that we pack a
+    few bits, then the next data type will flush that byte.'''
     if b:
       b = 1
     else:
       b = 0
-    shift = self.bitcount % 8
-    if shift == 0:
-      self.bits.append(0)
-    self.bits[-1] |= (b << shift)
-    self.bitcount += 1
+    shift = self._bitcount % 8
+    if shift == 0 and self._bitcount > 0:
+      self._flush_bits( stream )
+
+    self._bitfield |= (b << shift)
+    self._bitcount += 1
+
+  def _flush_bits(self, stream):
+    '''Flush the bits that have been packed into the current byte and reset state.'''
+    # Because this can be called many times when writing a table, be safe
+    if self._bitcount > 0:
+      stream.write(pack('B', self._bitfield))
+      self._bitfield = 0
+      self._bitcount = 0
 
   def write_octet(self, n):
     """
@@ -61,8 +95,14 @@ class Writer(object):
     """
     if (n < 0) or (n > 255):
       raise ValueError('Octet out of range 0..255')
-    self._flushbits()
-    self.out.write(pack('B', n))
+    self._output_buffer.append( (self._write_octet, n) )
+
+  def _write_octet(self, n, stream):
+    '''Write an octet to the output stream.'''
+    if (n < 0) or (n > 255):
+      raise ValueError('Octet out of range 0..255')
+    self._flush_bits( stream )
+    stream.write(pack('B', n))
 
   def write_short(self, n):
     """
@@ -70,105 +110,150 @@ class Writer(object):
     """
     if (n < 0) or (n > 65535):
       raise ValueError('Octet out of range 0..65535')
-    self._flushbits()
-    self.out.write(pack('>H', n))
+    self._output_buffer.append( (self._write_short, n) )
+
+  def _write_short(self, n, stream):
+    '''Write the short to the output stream.'''
+    if (n < 0) or (n > 65535):
+      raise ValueError('Octet out of range 0..65535')
+    self._flush_bits( stream )
+    stream.write(pack('>H', n))
 
   def write_long(self, n):
     """
     Write an integer as an unsigned2 32-bit value.
-
     """
     if (n < 0) or (n >= (2**32)):
       raise ValueError('Octet out of range 0..2**31-1')
-    self._flushbits()
-    self.out.write(pack('>I', n))
+    self._output_buffer.append( (self._write_long, n) )
+
+  def _write_long(self, n, stream):
+    '''Write the long to a stream.'''    
+    if (n < 0) or (n >= (2**32)):
+      raise ValueError('Octet out of range 0..2**31-1')
+    self._flush_bits( stream )
+    stream.write(pack('>I', n))
 
   def write_longlong(self, n):
     """
     Write an integer as an unsigned 64-bit value.
-
     """
     if (n < 0) or (n >= (2**64)):
       raise ValueError('Octet out of range 0..2**64-1')
-    self._flushbits()
-    self.out.write(pack('>Q', n))
+    self._output_buffer.append( (self._write_longlong, n) )
+
+  def _write_longlong(self, n, stream):
+    '''Write the longlong to a stream.'''
+    if (n < 0) or (n >= (2**64)):
+      raise ValueError('Octet out of range 0..2**64-1')
+    self._flush_bits( stream )
+    stream.write(pack('>Q', n))
 
   def write_shortstr(self, s):
     """
     Write a string up to 255 bytes long after encoding.  If passed
     a unicode string, encode as UTF-8.
-
     """
-    self._flushbits()
     if isinstance(s, unicode):
       s = s.encode('utf-8')
     if len(s) > 255:
       raise ValueError('String too long')
-    self.write_octet(len(s))
-    self.out.write(s)
+    self._output_buffer.append( (self._write_shortstr, s) )
+
+  def _write_shortstr(self, s, stream):
+    '''Write the string to the stream.'''
+    if len(s) > 255:
+      raise ValueError('String too long')
+    self._write_octet(len(s), stream) # also flushes bits
+    stream.write(s)
 
   def write_longstr(self, s):
     """
     Write a string up to 2**32 bytes long after encoding.  If passed
     a unicode string, encode as UTF-8.
-
     """
-    self._flushbits()
     if isinstance(s, unicode):
       s = s.encode('utf-8')
-    self.write_long(len(s))
-    self.out.write(s)
+    self._output_buffer.append( (self._write_longstr, s) )
+
+  def _write_longstr(self, s, stream):
+    '''Write the long string to the stream.'''
+    self._write_long(len(s), stream) # also flushes bits
+    stream.write(s)
 
   def write_table(self, d):
     """
     Write out a Python dictionary made of up string keys, and values
     that are strings, signed integers, Decimal, datetime.datetime, or
     sub-dictionaries following the same constraints.
-
     """
-    self._flushbits()
-    table_data = Writer()
+    self._output_buffer.append( (self._write_table, d) )
+
+  def _write_table(self, d, stream):
+    '''Write the table to an output stream.'''
+    # HACK: encoding of AMQP tables is broken because it requires the length of
+    # the /encoded/ data instead of the number of items.  To support streaming,
+    # fiddle with cursor position, rewinding to write the real length of the
+    # data.  Generally speaking, I'm not a fan of the AMQP encoding scheme, it
+    # could be much faster.
+    table_len_pos = stream.tell()
+    print "table len at ", table_len_pos
+    self._write_long( 0, stream )
+    table_data_pos = stream.tell()
+    print "table data at ", table_data_pos
+    
     for k, v in d.items():
       # 6 April 09 aaron - Don't send table key unless the data type is
       # supported. TODO: an OO way of doing this.
       if isinstance(v, basestring):
         if isinstance(v, unicode):
           v = v.encode('utf-8')
-        table_data.write_shortstr(k)
-        table_data.write('S')
-        table_data.write_longstr(v)
+        self._write_shortstr(k, stream)
+        stream.write('S')
+        self._write_longstr(v, stream)
       elif isinstance(v, (int, long)):
-        table_data.write_shortstr(k)
-        table_data.write('I')
-        table_data.write(pack('>i', v))
+        self._write_shortstr(k, stream)
+        stream.write('I')
+        stream.write(pack('>i', v))
       elif isinstance(v, Decimal):
-        table_data.write_shortstr(k)
-        table_data.write('D')
+        self._write_shortstr(k, stream)
+        stream.write('D')
         sign, digits, exponent = v.as_tuple()
         v = 0
         for d in digits:
           v = (v * 10) + d
         if sign:
           v = -v
-        table_data.write_octet(-exponent)
-        table_data.write(pack('>i', v))
+        self._write_octet(-exponent, stream)
+        stream.write(pack('>i', v))
       elif isinstance(v, datetime):
-        table_data.write_shortstr(k)
-        table_data.write('T')
-        table_data.write_timestamp(v)
-        ## FIXME: timezone ?
+        self._write_shortstr(k, stream)
+        stream.write('T')
+        self._write_timestamp(v, stream)
+        ## FIXME: timezone ?  
       elif isinstance(v, dict):
-        table_data.write_shortstr(k)
-        table_data.write('F')
-        table_data.write_table(v)
-    table_data = table_data.getvalue()
-    self.write_long(len(table_data))
-    self.out.write(table_data)
+        self._write_shortstr(k, stream)
+        stream.write('F')
+        self._write_table(v, stream)
+    #table_data = table_data.getvalue()
+    table_end_pos = stream.tell()
+    table_len = table_end_pos - table_data_pos
+    print "table end at ", table_end_pos, " len ", table_len
+    stream.seek( table_len_pos )
+    print "writing table len at ", stream.tell()
 
-  def write_timestamp(self, v):
+    self._write_long( table_len, stream )
+    print "finished writing table len at ", stream.tell()
+    stream.seek( 0, 2 )
+    print "stream back at pos ", stream.tell()
+
+  def write_timestamp(self, t):
     """
     Write out a Python datetime.datetime object as a 64-bit integer
     representing seconds since the Unix epoch.
-
     """
-    self.out.write(pack('>q', long(mktime(v.timetuple()))))
+    self._output_buffer.append( (self._write_timestamp, t) )
+
+  def _write_timestamp(self, v, stream):
+    '''Write the timestamp to a stream.'''
+    stream.write(pack('>q', long(mktime(v.timetuple()))))
---
+++ test/unit/writer_test.py
@@ -0,0 +1,54 @@
+
+import mox
+from datetime import datetime
+from cStringIO import StringIO
+
+from haigha.writer import Writer
+
+
+class WriterTest(mox.MoxTestBase):
+
+  def setUp(self):
+    mox.MoxTestBase.setUp( self )
+
+  def test_write_methods(self):
+    writer = Writer()
+    writer.write( 'foo' )
+    writer.write_bit( 1 )
+    writer.write_octet( 5 )
+    writer.write_short( 42 )
+    writer.write_long( 12345 )
+    writer.write_longlong( 123456789 )
+    writer.write_shortstr( "bar" )
+    writer.write_longstr( "hellowurld" )
+    writer.write_table( {'cats':'dogs'} )
+    writer.write_timestamp( 'now' )
+    
+    self.assertEquals( 10, len(writer._output_buffer) )
+    self.assertEquals( (writer._write_str, 'foo'), writer._output_buffer[0] )
+    self.assertEquals( (writer._write_bit, 1), writer._output_buffer[1] )
+    self.assertEquals( (writer._write_octet, 5), writer._output_buffer[2] )
+    self.assertEquals( (writer._write_short, 42), writer._output_buffer[3] )
+    self.assertEquals( (writer._write_long, 12345), writer._output_buffer[4] )
+    self.assertEquals( (writer._write_longlong, 123456789), writer._output_buffer[5] )
+    self.assertEquals( (writer._write_shortstr, 'bar'), writer._output_buffer[6] )
+    self.assertEquals( (writer._write_longstr, 'hellowurld'), writer._output_buffer[7] )
+    self.assertEquals( (writer._write_table, {'cats':'dogs'}), writer._output_buffer[8] )
+    self.assertEquals( (writer._write_timestamp, 'now'), writer._output_buffer[9] )
+
+  def test_flush(self):
+    # TODO: test the datatypes in more depth
+    writer = Writer()
+    writer.write( 'foo' )
+    writer.write_bit( 1 )
+    writer.write_octet( 5 )
+    writer.write_short( 42 )
+    writer.write_long( 12345 )
+    writer.write_longlong( 123456789 )
+    writer.write_shortstr( "bar" )
+    writer.write_longstr( "hellowurld" )
+    writer.write_table( {'cats':'dogs'} )
+    writer.write_timestamp( datetime.now() )
+
+    stream = StringIO()
+    writer.flush( stream )
