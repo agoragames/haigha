9f692bae1066fa6f70e66b437282d6a5b998d548
Vitaly Babiy <vbabiy@agoragames.com>
2010-09-24 11:49:08 -0400

    Content frame properties

--- lib/frames/content_frame.py
+++ lib/frames/content_frame.py
@@ -9,9 +9,22 @@ class ContentFrame(Frame):
   @classmethod
   def type(cls):
     return 3
+  
+  @property
+  def class_id(self):
+    return self._class_id
+  
+  @property
+  def weight(self):
+    return self._weight
+  
+  @property
+  def body_size(self):
+    return self._body_size
 
   def __init__(self, *args, **kwargs):
     Frame.__init__(self, *args, **kwargs)
+    self._class_id, self._weight, self._body_size = struct.unpack( '>HHQ', payload[:12] )
 
 ContentFrame.register()
 
