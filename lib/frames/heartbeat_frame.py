daebd3e10e5587aaa5b406c5f6414032232e92fe
Vitaly Babiy <vbabiy@agoragames.com>
2010-09-24 08:49:09 -0400

    Notes about heartbeat type

--- lib/frames/heartbeat_frame.py
+++ lib/frames/heartbeat_frame.py
@@ -8,6 +8,9 @@ class HeartbeatFrame(Frame):
 
   @classmethod
   def type():
+    # NOTE: The PDF spec say this should be 4 but the xml spec say it should be 8
+    # PDF spec: http://www.amqp.org/confluence/download/attachments/720900/amqp0-9-1.pdf?version=1&modificationDate=1227526523000
+    # XML spec: http://www.amqp.org/confluence/download/attachments/720900/amqp0-9-1.xml?version=1&modificationDate=1227526672000
     return 8
   register()
 
