cf747a40b59770b7e0cbc21da30f6179bf4f81c4
Vitaly Babiy <vbabiy@agoragames.com>
2010-09-24 11:38:17 -0400

    added method frame properties

--- lib/frames/method_frame.py
+++ lib/frames/method_frame.py
@@ -10,11 +10,22 @@ class MethodFrame(Frame):
   @classmethod
   def type(cls):
     return 1
-
+  
+  @property
+  def class_id(self):
+    return self._class_id
+  
+  @property
+  def method_id(self):
+    return self._method_id
+  
+  @property
+  def args(self):
+    return self._args
 
   def __init__(self, *args, **kwargs):
     Frame.__init__(self, *args, **kwargs)
-    self.class_id, self.method_id = struct.unpack( '>HH', self.payload[:4] )
-    self.args = Reader( self.payload[4:] )
+    self._class_id, self._method_id = struct.unpack( '>HH', self.payload[:4] )
+    self._args = Reader( self.payload[4:] )
   
 MethodFrame.register()
