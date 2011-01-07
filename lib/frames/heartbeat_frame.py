3ecd0831aac1c9cd659c526837b1ef7e5b910dd8
Aaron Westendorf <aaron@agoragames.com>
2011-01-07 16:38:43 -0500

    Confirmed type value for 0.9.1 protocol

--- lib/frames/heartbeat_frame.py
+++ lib/frames/heartbeat_frame.py
@@ -12,6 +12,7 @@ class HeartbeatFrame(Frame):
     # NOTE: The PDF spec say this should be 4 but the xml spec say it should be 8
     #       RabbitMQ seems to implement this as 8, but maybe that's a difference
     #       between 0.8 and 0.9 protocols
+    # Using Rabbit 2.1.1 and protocol 0.9.1 it seems that 8 is indeed the correct type @AW
     # PDF spec: http://www.amqp.org/confluence/download/attachments/720900/amqp0-9-1.pdf?version=1&modificationDate=1227526523000
     # XML spec: http://www.amqp.org/confluence/download/attachments/720900/amqp0-9-1.xml?version=1&modificationDate=1227526672000
     return 8
