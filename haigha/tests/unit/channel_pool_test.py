b208f8a28d4289d97aa529d5ad8048a514179029
Aaron Westendorf <aaron@agoragames.com>
2011-01-25 16:18:52 -0500

    Unit tests for channel pool [#1 state:resolved]

--- haigha/tests/unit/channel_pool_test.py
+++ haigha/tests/unit/channel_pool_test.py
@@ -8,3 +8,59 @@ class ChannelPoolTest(mox.MoxTestBase):
     c = ChannelPool('connection')
     self.assertEquals('connection', c._connection)
     self.assertEquals(set(), c._free_channels)
+
+  def test_publish_without_user_cb(self):
+    ch = self.create_mock_anything()
+    ch.__hash__ = lambda: 42
+    cp = ChannelPool(None)
+    self.mock( cp, '_get_channel' )
+
+    def test_committed_cb(cb):
+      cb()
+      return True
+
+    cp._get_channel().AndReturn( ch )
+    ch.publish_synchronous( 'arg1', 'arg2', cb=mox.Func(test_committed_cb), doit='harder' )
+
+    self.replay_all()
+    self.assertEquals( set(), cp._free_channels )
+    cp.publish( 'arg1', 'arg2', doit='harder' )
+    self.assertEquals( set([ch]), cp._free_channels )
+
+  def test_publish_with_user_cb(self):
+    ch = self.create_mock_anything()
+    ch.__hash__ = lambda: 42
+    cp = ChannelPool(None)
+    self.mock( cp, '_get_channel' )
+    user_cb = self.create_mock_anything()
+
+    def test_committed_cb(cb):
+      cb()
+      return True
+
+    cp._get_channel().AndReturn( ch )
+    ch.publish_synchronous( 'arg1', 'arg2', cb=mox.Func(test_committed_cb), doit='harder' )
+    user_cb()
+
+    self.replay_all()
+    self.assertEquals( set(), cp._free_channels )
+    cp.publish( 'arg1', 'arg2', cb=user_cb, doit='harder' )
+    self.assertEquals( set([ch]), cp._free_channels )
+
+  def test_get_channel_when_none_free(self):
+    conn = self.create_mock_anything()
+    cp = ChannelPool(conn)
+
+    conn.channel().AndReturn( 'channel' )
+    
+    self.replay_all()
+    self.assertEquals( 'channel', cp._get_channel() )
+    self.assertEquals( set(), cp._free_channels )
+
+  def test_get_channel_when_one_free(self):
+    conn = self.create_mock_anything()
+    cp = ChannelPool(conn)
+    cp._free_channels = set(['channel'])
+
+    self.assertEquals( 'channel', cp._get_channel() )
+    self.assertEquals( set(), cp._free_channels )
