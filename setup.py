776b9f6896f75bbb93a486697d29da4a92f74bb1
Aaron Westendorf <aaron@agoragames.com>
2011-05-12 15:48:23 -0400

    Same license and similar setup as chai

--- setup.py
+++ setup.py
@@ -1,34 +1,24 @@
 import haigha
 import os
+from distutils.core import setup
 
-try:
-    from setuptools import setup, find_packages, Command
-    from setuptools.command.test import test
-    from setuptools.command.install import install
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages, Command
-    from setuptools.command.test import test
-    from setuptools.command.install import install
 
 requirements = map(str.strip, open('requirements.txt').readlines())
 
 setup(
-    name='Haigha',
+    name='haigha',
     version=haigha.VERSION,
-    author_email="""
-        "Aaron Westendorf" <aaron@agoragames.com>,
-        "Vitaly Babiy" <vbabiy@agoragames.com>"
-    """,
-    packages = find_packages(),
+    author='Vitaly Babiy, Aaron Westendorf',
+    author_email="vbabiy@agoragames.com, aaron@agoragames.com",
+    packages = ['haigha'],
     install_requires = requirements,
-    license="MIT License",
+    url='https://github.com/agoragames/haigha',
+    license="LICENSE.txt",
     long_description=open('README.txt').read(),
-    keywords=['amqp', 'evented'],
+    keywords=['python', 'amqp', 'event', 'rabbitmq'],
     classifiers=[
         'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: BSD License',
         "Intended Audience :: Developers",
         "Operating System :: POSIX",
         "Topic :: Communications",
