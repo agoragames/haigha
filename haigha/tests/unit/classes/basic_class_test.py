4d49361403e5ae109fa0df885295f272c8586ed4
Steven Bronson <sbronson@agoragames.com>
2011-06-02 11:31:21 -0400

    For BasicClass, changed tests to follow the style of the others and deleted redundant tests.

--- haigha/tests/unit/classes/basic_class_test.py
+++ haigha/tests/unit/classes/basic_class_test.py
@@ -3,8 +3,11 @@ from chai import Chai
 from haigha.classes import basic_class, ProtocolClass, BasicClass
 from haigha.frames import MethodFrame
 from haigha.writer import Writer
+from haigha.reader import Reader
 from haigha.message import Message
 from haigha.connection import Connection
+
+from collections import deque
 
 class BasicClassTest(Chai):
 
@@ -44,7 +47,7 @@ class BasicClassTest(Chai):
     expect( writer.write_long ).args( 0 )
     expect( writer.write_short ).args( 0 )
     expect( writer.write_bit ).args( False )
-    expect( mock( basic_class, 'MethodFrame' ) ).args( self.klass.channel_id, 60, 10, writer ).returns( 'frame' )
+    expect( mock( basic_class, 'MethodFrame' ) ).args( 42, 60, 10, writer ).returns( 'frame' )
     expect( self.klass.send_frame ).args( 'frame' )
     expect( self.klass.channel.add_synchronous_cb ).args( self.klass._recv_qos_ok )
 
@@ -56,7 +59,7 @@ class BasicClassTest(Chai):
     expect( writer.write_long ).args( 1 )
     expect( writer.write_short ).args( 2 )
     expect( writer.write_bit ).args( 3 )
-    expect( mock( basic_class, 'MethodFrame' ) ).args( self.klass.channel_id, 60, 10, writer ).returns( 'frame' )
+    expect( mock( basic_class, 'MethodFrame' ) ).args( 42, 60, 10, writer ).returns( 'frame' )
     expect( self.klass.send_frame ).args( 'frame' )
     expect( self.klass.channel.add_synchronous_cb ).args( self.klass._recv_qos_ok )
 
@@ -74,7 +77,7 @@ class BasicClassTest(Chai):
     expect( writer.write_shortstr ).args( 'ctag' )
     expect( writer.write_bits ).args( False, True, False, True )
     expect( writer.write_table ).args( {} )
-    expect( mock(basic_class,'MethodFrame') ).args(self.klass.channel_id, 60, 20, writer).returns( 'frame' )
+    expect( mock(basic_class,'MethodFrame') ).args(42, 60, 20, writer).returns( 'frame' )
     expect( self.klass.send_frame ).args( 'frame' )
     #expect( self.klass.channel.add_synchronous_cb ).args( self.klass._recv_consume_ok )
 
@@ -92,7 +95,7 @@ class BasicClassTest(Chai):
     expect( writer.write_shortstr ).args( 'stag' )
     expect( writer.write_bits ).args( 'nloc', 'nack', 'mine', False )
     expect( writer.write_table ).args( {} )
-    expect( mock(basic_class,'MethodFrame') ).args(self.klass.channel_id, 60, 20, writer).returns( 'frame' )
+    expect( mock(basic_class,'MethodFrame') ).args(42, 60, 20, writer).returns( 'frame' )
     expect( self.klass.send_frame ).args( 'frame' )
     expect( self.klass.channel.add_synchronous_cb ).args( self.klass._recv_consume_ok )
 
@@ -112,7 +115,7 @@ class BasicClassTest(Chai):
     expect( writer.write_shortstr ).args( 'stag' )
     expect( writer.write_bits ).args( 'nloc', 'nack', 'mine', False )
     expect( writer.write_table ).args( {} )
-    expect( mock(basic_class,'MethodFrame') ).args(self.klass.channel_id, 60, 20, writer).returns( 'frame' )
+    expect( mock(basic_class,'MethodFrame') ).args(42, 60, 20, writer).returns( 'frame' )
     expect( self.klass.send_frame ).args( 'frame' )
     expect( self.klass.channel.add_synchronous_cb ).args( self.klass._recv_consume_ok )
 
@@ -133,86 +136,13 @@ class BasicClassTest(Chai):
     assert_equals( {'ctag':'consumer'}, self.klass._consumer_cb )
     assert_equals( [], self.klass._pending_consumers )
 
-  def test_cancel_default_args0(self):
-    writer = mock()
-    expect( mock(basic_class, 'MethodFrame') ).args(self.klass.channel_id, 60, 30, ignore()).returns( 'frame' )
-    expect(self.klass.send_frame).args('frame')
-    expect(self.klass.logger.warning).args(ignore(), ignore())
-    self.klass.cancel()
-
-  def test_cancel_consumer_tag(self):
-    writer = mock()
-    expect( mock(basic_class, 'MethodFrame') ).args(self.klass.channel_id, 60, 30, ignore()).returns( 'frame' )
-    expect(self.klass.send_frame).args('frame')
-    expect(self.klass.logger.warning).args(ignore(), ignore())
-    self.klass.cancel(self.sample_tag)
-
-  def test_cancel_existing_consumer_tag(self):
-    expect( mock(basic_class, 'MethodFrame') ).args(self.klass.channel_id, 60, 30, ignore()).returns( 'frame' )
-    self.klass._consumer_cb[self.sample_tag] = object()
-    expect(self.klass.send_frame).args('frame')
-    self.klass.cancel(self.sample_tag)
-    assertTrue(self.sample_tag not in self.klass._consumer_cb)
-
-  def test_cancel_consumer_no_cb(self):
-    expect( mock(basic_class, 'MethodFrame') ).args(self.klass.channel_id, 60, 30, ignore()).returns( 'frame' )
-    expect(self.klass.send_frame).args('frame')
-    expect(self.klass.logger.warning).args(ignore(), ignore())
-    self.klass.cancel(consumer=object())
-
-  def test_cancel_consumer_with_cb(self):
-    consumer = object()
-    self.klass._consumer_cb[self.sample_tag] = consumer
-    expect( mock(basic_class, 'MethodFrame') ).args(self.klass.channel_id, 60, 30, ignore()).returns( 'frame' )
-    expect(self.klass.send_frame).args('frame')
-    self.klass.cancel(consumer_tag=self.sample_tag, consumer=consumer)
-    assertTrue(self.sample_tag not in self.klass._consumer_cb)
-    assertTrue(consumer not in self.klass._consumer_cb.values())
-
-  def test_cancel_nowait_false(self):
-    f = lambda:None
-    expect( mock(basic_class, 'MethodFrame') ).args(self.klass.channel_id, 60, 30, ignore()).returns( 'frame' )
-    expect(self.klass.send_frame).args('frame')
-    expect(self.klass.channel.add_synchronous_cb).args(self.klass._recv_cancel_ok)
-    self.klass.cancel(nowait=False, cb=f)
-    assertTrue(f in self.klass._cancel_cb)
-
-  def test_cancel_nowait_false_no_cb(self):
-    expect( mock(basic_class, 'MethodFrame') ).args(self.klass.channel_id, 60, 30, ignore()).returns( 'frame' )
-    expect(self.klass.send_frame).args('frame')
-    expect(self.klass.channel.add_synchronous_cb).args(self.klass._recv_cancel_ok)
-    self.klass.cancel(nowait=False)
-    assertTrue(None in self.klass._cancel_cb)
-
-  def test__recv_cancel_ok(self):
-    frame = mock()
-    expect(frame.args.read_shortstr).returns(self.sample_tag)
-    expect(self.klass.logger.warning).args(ignore(), ignore())
-    self.klass._recv_cancel_ok(frame)
-
-  def test__recv_cancel_ok_with_cb(self):
-    # python sucks
-    i = []
-    def f():
-      i.append(0)
-
-    frame = mock()
-    self.klass._consumer_cb[self.sample_tag] = f
-    self.klass._cancel_cb.append(f)
-    expect(frame.args.read_shortstr).returns(self.sample_tag)
-    assertEquals(i, [])
-    self.klass._recv_cancel_ok(frame)
-    assertEquals(i, [0])
-    assertTrue(f not in self.klass._consumer_cb)
-    assertTrue(f not in self.klass._cancel_cb)
-
-  def test_cancel_default_args1(self):
+  def test_cancel_default_args(self):
     writer = mock()
     expect( mock(basic_class, 'Writer') ).returns( writer )
     expect( writer.write_shortstr ).args( '' )
     expect( writer.write_bit ).args( True )
     
-    expect( mock(basic_class,'MethodFrame') ).args(self.klass.channel_id, 60, 30, writer).returns( 'frame' )
+    expect( mock(basic_class,'MethodFrame') ).args(42, 60, 30, writer).returns( 'frame' )
     expect( self.klass.send_frame ).args( 'frame' )
     
     self.klass._consumer_cb[ '' ] = 'foo'
@@ -227,7 +157,7 @@ class BasicClassTest(Chai):
     expect( writer.write_shortstr ).args( 'ctag' )
     expect( writer.write_bit ).args( True )
     
-    expect( mock(basic_class,'MethodFrame') ).args(self.klass.channel_id, 60, 30, writer).returns( 'frame' )
+    expect( mock(basic_class,'MethodFrame') ).args(42, 60, 30, writer).returns( 'frame' )
     expect( self.klass.send_frame ).args( 'frame' )
 
     expect( self.klass.logger.warning ).args( 
@@ -244,7 +174,7 @@ class BasicClassTest(Chai):
     expect( writer.write_shortstr ).args( '' )
     expect( writer.write_bit ).args( False )
     
-    expect( mock(basic_class,'MethodFrame') ).args(self.klass.channel_id, 60, 30, writer).returns( 'frame' )
+    expect( mock(basic_class,'MethodFrame') ).args(42, 60, 30, writer).returns( 'frame' )
     expect( self.klass.send_frame ).args( 'frame' )
 
     expect( self.klass.channel.add_synchronous_cb ).args( self.klass._recv_cancel_ok )
@@ -260,7 +190,7 @@ class BasicClassTest(Chai):
     expect( writer.write_shortstr ).args( '' )
     expect( writer.write_bit ).args( False )
     
-    expect( mock(basic_class,'MethodFrame') ).args(self.klass.channel_id, 60, 30, writer).returns( 'frame' )
+    expect( mock(basic_class,'MethodFrame') ).args(42, 60, 30, writer).returns( 'frame' )
     expect( self.klass.send_frame ).args( 'frame' )
 
     expect( self.klass.channel.add_synchronous_cb ).args( self.klass._recv_cancel_ok )
@@ -276,7 +206,7 @@ class BasicClassTest(Chai):
     expect( writer.write_shortstr ).args( 'ctag' )
     expect( writer.write_bit ).args( True )
     
-    expect( mock(basic_class,'MethodFrame') ).args(self.klass.channel_id, 60, 30, writer).returns( 'frame' )
+    expect( mock(basic_class,'MethodFrame') ).args(42, 60, 30, writer).returns( 'frame' )
     expect( self.klass.send_frame ).args( 'frame' )
 
     self.klass._consumer_cb[ 'ctag' ] = 'consumer'
@@ -304,29 +234,94 @@ class BasicClassTest(Chai):
 
     self.klass._recv_cancel_ok( frame )
 
-  def publish_helper(self, ticket):
+  def test_publish_default_args(self):
     args = Writer()
-    exchange = 'exchange'
-    routing_key = 'routing_key'
     msg = Message('hello, world')
-    args.write_short(ticket if ticket is not None else self.klass.default_ticket)\
-        .write_shortstr(exchange)\
-        .write_shortstr(routing_key)\
+    args.write_short(0)\
+        .write_shortstr('exchange')\
+        .write_shortstr('routing_key')\
         .write_bits(False, False)
-    expect( mock(basic_class, 'MethodFrame') ).args(self.klass.channel_id, 60, 40, args).returns( 'methodframe' )
-    expect( mock(basic_class, 'HeaderFrame') ).args(self.klass.channel_id, 60, 0, len(msg), msg.properties).returns( 'headerframe' )
-    frame_max = 3
-    self.klass.channel.connection.frame_max = frame_max
-    frames = ['f0', 'f1', 'f2']
-    expect(mock(basic_class, 'ContentFrame').create_frames).args(self.klass.channel_id, msg.body, frame_max).returns(frames)
+    self.klass.channel.connection.frame_max = 3
+    expect( mock(basic_class, 'MethodFrame') ).args(42, 60, 40, args).returns( 'methodframe' )
+    expect( mock(basic_class, 'HeaderFrame') ).args(42, 60, 0, len(msg), msg.properties).returns( 'headerframe' )
+    expect(mock(basic_class, 'ContentFrame').create_frames).args(42, msg.body, 3).returns(['f0', 'f1', 'f2'])
     expect(self.klass.send_frame).args('methodframe')
     expect(self.klass.send_frame).args('headerframe')
-    for frame in frames:
-      expect(self.klass.send_frame).args(frame)
-    self.klass.publish(msg, exchange, routing_key, ticket=ticket)
-
-  def test_publish_default_args(self):
-    self.publish_helper(None)
+    expect(self.klass.send_frame).args('f0')
+    expect(self.klass.send_frame).args('f1')
+    expect(self.klass.send_frame).args('f2')
+    self.klass.publish(msg, 'exchange', 'routing_key', ticket=0)
 
   def test_publish_with_ticket(self):
-    self.publish_helper(3)
+    args = Writer()
+    msg = Message('hello, world')
+    args.write_short(3)\
+        .write_shortstr('exchange')\
+        .write_shortstr('routing_key')\
+        .write_bits(False, False)
+    expect( mock(basic_class, 'MethodFrame') ).args(42, 60, 40, args).returns( 'methodframe' )
+    expect( mock(basic_class, 'HeaderFrame') ).args(42, 60, 0, len(msg), msg.properties).returns( 'headerframe' )
+    self.klass.channel.connection.frame_max = 3
+    expect(mock(basic_class, 'ContentFrame').create_frames)\
+      .args(42, msg.body, 3).returns(['f0', 'f1', 'f2'])
+    expect(self.klass.send_frame).args('methodframe')
+    expect(self.klass.send_frame).args('headerframe')
+    expect(self.klass.send_frame).args('f0')
+    expect(self.klass.send_frame).args('f1')
+    expect(self.klass.send_frame).args('f2')
+    self.klass.publish(msg, 'exchange', 'routing_key', ticket=3)
+
+  def test_return_msg(self):
+    method_frame = mock()
+    args = Writer()
+    args.write_short(3)
+    args.write_shortstr('reply_text')
+    args.write_shortstr('exchange')
+    args.write_shortstr('routing_key')
+    expect(mock(basic_class, 'MethodFrame')).args(42, 60, 50, args).returns(method_frame)
+    expect(self.klass.send_frame).args(method_frame)
+    self.klass.return_msg(3, 'reply_text', 'exchange', 'routing_key')
+
+  def test__recv_return(self):
+    pass
+
+  def test__recv_deliver_no_frames(self):
+    expect(self.klass.channel.next_frame).returns(None)
+    expect(self.klass.channel.requeue_frames).args(['method_frame'])
+    assert_raises(self.klass.FrameUnderflow, self.klass._recv_deliver, 'method_frame')
+
+  def test__recv_deliver_underflow(self):
+    header_frame = mock()
+    header_frame.size = 1000000
+    expect(self.klass.channel.next_frame).returns(header_frame)
+    expect(self.klass.channel.next_frame).returns(None)
+    expect(self.klass.channel.requeue_frames).args(deque([header_frame, 'method_frame']))
+    assert_raises(self.klass.FrameUnderflow, self.klass._recv_deliver, 'method_frame')
+
+  def test__recv_deliver_no_cb(self):
+    i = []
+    def f():
+      i.append(0)
+
+    args = mock()
+    method_frame = mock()
+    header_frame = mock()
+    header_frame.size = 100
+    header_frame.properties = {}
+    frame = mock()
+    delivery_info = {'channel': self.klass.channel,
+                     'consumer_tag': 'consumer_tag',
+                     'delivery_tag': 9,
+                     'redelivered': False,
+                     'exchange': 'exchange',
+                     'routing_key': 'routing_key'}
+    expect(self.klass.channel.next_frame).returns(header_frame)
+    expect(self.klass.channel.next_frame).returns(frame)
+    expect(frame.payload.buffer).returns('x'*100)
+    expect(method_frame.args.read_shortstr).returns('consumer_tag')
+    expect(method_frame.args.read_longlong).returns(9)
+    expect(method_frame.args.read_bit).returns(False)
+    expect(method_frame.args.read_shortstr).returns('exchange')
+    expect(method_frame.args.read_shortstr).returns('routing_key')
+    expect(mock(basic_class, 'Message')).args(body=bytearray('x'*100), delivery_info=delivery_info, **header_frame.properties)
+    self.klass._recv_deliver(method_frame)
