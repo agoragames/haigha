641b9886ac0781a0b4b554e8fbd4aef185cd065e
Aaron Westendorf <aaron.westendorf@gmail.com>
2011-11-14 12:34:45 -0500

    Added test to ensure that queue is left alone if channel is inactive after a publish

--- haigha/tests/unit/channel_pool_test.py
+++ haigha/tests/unit/channel_pool_test.py
@@ -90,6 +90,31 @@ class ChannelPoolTest(Chai):
     self.assertEquals( set([ch]), cp._free_channels )
     assert_equals( deque(), cp._queue )
 
+  def test_publish_does_not_resend_queued_messages_if_channel_is_inactive(self):
+    ch = mock()
+    cp = ChannelPool(None)
+    user_cb = mock()
+    ch.active = True
+    cp._queue.append( (('a1', 'a2'), {'cb':'foo', 'yo':'dawg'}) )
+
+    def test_committed_cb(cb):
+      # Because using this for side effects is kinda fugly, protect it
+      if not getattr(cb,'_called_yet',False):
+        ch.active = False
+        expect(user_cb)
+        cb()
+        setattr(cb, '_called_yet', True)
+      
+      return True
+
+    expect(cp._get_channel).returns( ch )
+    expect(ch.publish_synchronous).args( 'arg1', 'arg2', cb=func(test_committed_cb), doit='harder' )
+
+    self.assertEquals( set(), cp._free_channels )
+    cp.publish( 'arg1', 'arg2', cb=user_cb, doit='harder' )
+    self.assertEquals( set([ch]), cp._free_channels )
+    assert_equals( 1, len(cp._queue) )
+
   def test_publish_searches_for_active_channel(self):
     ch1 = mock()
     ch2 = mock()
