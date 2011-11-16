8ca022f23ced3103ef5fabd9758a0d66213c6b35
Aaron Westendorf <aaron@agoragames.com>
2011-11-16 13:40:12 -0500

    Have to include new transports module in build

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     version=haigha.VERSION,
     author='Vitaly Babiy, Aaron Westendorf',
     author_email="vbabiy@agoragames.com, aaron@agoragames.com",
-    packages = ['haigha', 'haigha.frames', 'haigha.classes'],
+    packages = ['haigha', 'haigha.frames', 'haigha.classes', 'haigha.transports'],
     install_requires = requirements,
     url='https://github.com/agoragames/haigha',
     license="LICENSE.txt",
